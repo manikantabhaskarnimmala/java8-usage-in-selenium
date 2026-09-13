291756bf85bee3795bb5f74e53237fc7a4d4c31d
Kiran <kiran.nimmala2005@gmail.com>
2026-09-13 23:34:59 +0530

    "updated oop programs."

--- src/test/java/oopExamples/Abstraction.java
+++ src/test/java/oopExamples/Abstraction.java
@@ -2,18 +2,18 @@ package oopExamples;
 
 /*
  * Abstraction means hiding implementation details and exposing only the required functionality. 
- * Here, the Browser interface defines the launch() method, but it doesn't define how the browser is launched. 
- * Chrome and Firefox provide their own implementations.
+ * Here, the WebDriver interface defines the launch() method, but it doesn't define how the browser is launched. 
+ * ChromeBrowser and FirefoxBrowser provide their own implementations.
  */
 
 interface WebDriver {
-	public void get();
+	public void launch();
 }
 
 class ChromeBrowser implements WebDriver {
 
 	@Override
-	public void get() {
+	public void launch() {
 		System.out.println("Launched chrome browser");
 	}
 }
@@ -21,7 +21,7 @@ class ChromeBrowser implements WebDriver {
 class EdgeBrowser implements WebDriver {
 
 	@Override
-	public void get() {
+	public void launch() {
 		System.out.println("Launched edge browser");
 	}
 }
@@ -30,8 +30,12 @@ public class Abstraction {
 
 	public static void main(String[] args) {
 
-		WebDriver driver = new ChromeBrowser();
-		driver.get();
+		WebDriver driver ;
+		driver= new ChromeBrowser();
+		driver.launch();
+		
+		driver = new EdgeBrowser();
+		driver.launch();
 
 	}
 
--- src/test/java/oopExamples/Polymorphism.java
+++ src/test/java/oopExamples/Polymorphism.java
@@ -3,7 +3,8 @@ package oopExamples;
 /*
  * Polymorphism means one reference can represent different forms of objects. 
  * Here, the Browser reference points to a Chrome object first and a Firefox object later. 
- * Both child classes override the launch() method. At runtime, Java decides which implementation to execute based on the actual object.
+ * Both child classes override the launch() method from parent Browser class. 
+ * At runtime, Java decides which implementation to execute based on the actual object.
  */
 
 class Browser {
@@ -37,7 +38,7 @@ public class Polymorphism {
 		driver = new Chrome(); // Runtime Polymorphism
 		driver.launch();
 
-		driver = new Firefox();
+		driver = new Firefox(); // Runtime Polymorphism
 		driver.launch();
 
 	}
